aa8f57a59f878dd58d641d638d9c08c2d2f17b9a
pholpaphankorn <phol12711@outlook.com>
2023-10-26 22:53:09 +0700

    added navbar, fixed scrolling bugs, added textarea dynamic rows

--- app/page.tsx
+++ app/page.tsx
@@ -1,26 +1,30 @@
+
 import { ChatWindow } from "@/components/ChatWindow";
-
-
+import { Navbar } from "@/components/Navbar";
+import Link from 'next/link';
 export default function Home() {
-  const InfoCard = (
-    <div className="p-4 md:p-8 rounded bg-[#25252d] w-full max-h-[85%] overflow-hidden">
-      <h1 className="text-3xl md:text-4xl mb-4">
-        Hi, I&apos;m Vanaj.
-      </h1>
-      <p>
-      I&apos;m a close friend of Paphankorn Tanaroj. I&apos;m here to answer any questions you might have. Don&apos;t hesitate to reach out!
-      </p>
-    </div>
-  );
-  const id = 'phol'
+
   return (
-    <ChatWindow
-      endpoint="api/chat/retrieval"
-      titleText="Vanaj"
-      placeholder="Ask me anything!"
-      emptyStateComponent=""
-    ></ChatWindow>
+    <div>
+      <div className="p-4 md:p-8 rounded bg-[#25252d] w-full max-h-[85%] overflow-hidden">
+        <h1 className="text-3xl md:text-4xl mb-4">
+          Hi, I&apos;m Vanaj.
+        </h1>
+        <p>
+          I&apos;m a close friend of Paphankorn Tanaroj. I&apos;m here to answer any questions you might have. Don&apos;t hesitate to reach out!
+        </p>
+      </div>
+      <Link href="/chat">
+        <button>
+          Start
+        </button>
+      </Link>
+    </div>
+   
+  
 
 
   );
 }
+
+
