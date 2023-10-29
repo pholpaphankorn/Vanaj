e601b5c7b4954c65ffe2410c00d39dee2f3e64f2
pholpaphankorn <phol12711@outlook.com>
2023-10-29 17:25:51 +0700

    make ui responsive

--- components/Navbar.tsx
+++ components/Navbar.tsx
@@ -4,11 +4,11 @@ export function Navbar(props: { chat: Boolean }) {
 
   return (
 
-<nav className="bg-white fixed w-full z-20">
+<nav className="bg-white fixed w-full z-20 ">
   <div className="mx-auto max-w-7xl px-2 sm:px-6 lg:px-8 ">
-    <div className="relative flex h-16 items-center justify-between">
+    <div className="relative flex navbar-height items-center justify-between">
     {props.chat ? <ProfileCard active={true} /> :
-<h3 className="text-xl font-bold text-black">
+<h3 className="vanaj-logo-txt font-bold text-black ml-1">
    Vanaj
 </h3>}
     </div>
