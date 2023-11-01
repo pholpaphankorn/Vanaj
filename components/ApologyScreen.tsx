72f95b88b1c751ae5225cd62d1fc269e96c1dcaf
pholpaphankorn <phol12711@outlook.com>
2023-11-01 23:24:54 +0700

    change hero section a bit

--- components/ApologyScreen.tsx
+++ components/ApologyScreen.tsx
@@ -4,7 +4,7 @@ const ApologyScreen = () => {
     return (
         <div className="apology-container">
         <div className="apology-icon">😟</div>
-        <p className="apology-message">We&apos;re sorry, For some reason, Vanaj doesn&apos;t want to talk to you. Please try again later.</p>
+        <p className="apology-message">We&apos;re sorry, but for some reason, Vanaj doesn&apos;t want to talk to you. Please try again later.</p>
       </div>
     );
 };
