582f9873d91767538752b76c4ceb3ec625f656f9
pholpaphankorn <phol12711@outlook.com>
2023-11-20 08:11:25 +0700

    uncomment ingest

--- app/api/retrieval/ingest/route.ts
+++ app/api/retrieval/ingest/route.ts
@@ -20,57 +20,57 @@ export const runtime = "nodejs";
  * https://js.langchain.com/docs/modules/data_connection/document_transformers/text_splitters/recursive_text_splitter
  * https://js.langchain.com/docs/modules/data_connection/vectorstores/integrations/supabase
  */
-// export async function POST(req: NextRequest) {
-//   const data = await req.formData()
-//   const blob: Blob | null = data.get('file') as unknown as Blob
-//   const file_extension = data.get('file_extension') as unknown as string
-//   if (!blob) {
-//     return NextResponse.json({ success: false }, { status: 500 })
-//   }
+export async function POST(req: NextRequest) {
+  const data = await req.formData()
+  const blob: Blob | null = data.get('file') as unknown as Blob
+  const file_extension = data.get('file_extension') as unknown as string
+  if (!blob) {
+    return NextResponse.json({ success: false }, { status: 500 })
+  }
 
-//   try {
-//     const client = createClient(
-//       process.env.SUPABASE_URL!,
-//       process.env.SUPABASE_PRIVATE_KEY!,
-//     );
-//     var loader;
-//     if (file_extension=='txt'){
-//      loader = new TextLoader(blob);
-//     }else if (file_extension=='pdf'){
-//      loader = new PDFLoader(blob,{
-//       splitPages: false,
-//      });
-//     }else if (file_extension=='docx'){
-//      loader = new DocxLoader(blob);
-//     }else{
-//     return NextResponse.json({ error: 'File extension not supported' }, { status: 500 });
-//     }
-//     if (loader==null){
-//       return NextResponse.json({ error: 'Failed loading file' }, { status: 500 });
-//     }
-//     const docs = await loader.load();
+  try {
+    const client = createClient(
+      process.env.SUPABASE_URL!,
+      process.env.SUPABASE_PRIVATE_KEY!,
+    );
+    var loader;
+    if (file_extension=='txt'){
+     loader = new TextLoader(blob);
+    }else if (file_extension=='pdf'){
+     loader = new PDFLoader(blob,{
+      splitPages: false,
+     });
+    }else if (file_extension=='docx'){
+     loader = new DocxLoader(blob);
+    }else{
+    return NextResponse.json({ error: 'File extension not supported' }, { status: 500 });
+    }
+    if (loader==null){
+      return NextResponse.json({ error: 'Failed loading file' }, { status: 500 });
+    }
+    const docs = await loader.load();
 
-//     const splitter = new RecursiveCharacterTextSplitter({
-//       chunkSize: 256,
-//       chunkOverlap: 20,
-//     });
+    const splitter = new RecursiveCharacterTextSplitter({
+      chunkSize: 256,
+      chunkOverlap: 20,
+    });
 
-//     const splitDocuments = await splitter.splitDocuments(docs);
+    const splitDocuments = await splitter.splitDocuments(docs);
 
 
 
-//     const vectorstore = await SupabaseVectorStore.fromDocuments(
-//       splitDocuments,
-//       new OpenAIEmbeddings(),
-//       {
-//         client,
-//         tableName: "documents",
-//         queryName: "match_documents",
-//       },
-//     );
+    const vectorstore = await SupabaseVectorStore.fromDocuments(
+      splitDocuments,
+      new OpenAIEmbeddings(),
+      {
+        client,
+        tableName: "documents",
+        queryName: "match_documents",
+      },
+    );
 
-//     return NextResponse.json({ ok: true }, { status: 200 });
-//   } catch (e: any) {
-//     return NextResponse.json({ error: e.message }, { status: 500 });
-//   }
-// }
+    return NextResponse.json({ ok: true }, { status: 200 });
+  } catch (e: any) {
+    return NextResponse.json({ error: e.message }, { status: 500 });
+  }
+}
