92d3db6bfd16c6b67d106e156de6bdc26d21fd68
pholpaphankorn <phol12711@outlook.com>
2023-11-29 18:19:43 +0700

    removed stream response,added hyperlink in message

--- components/ChatMessageBubble.tsx
+++ components/ChatMessageBubble.tsx
@@ -1,13 +1,21 @@
 import type { Message } from "ai/react";
-import {CircularProfile} from "@/components/CircularProfile";
+import { CircularProfile } from "@/components/CircularProfile";
+import Linkify from "linkify-react";
 
-export function ChatMessageBubble(props: { message: Message}) {
+export function ChatMessageBubble(props: { message: Message }) {
   const colorClassName =
     props.message.role === "user" ? "chat-message-user-color" : "chat-message-vanaj-color ";
   const alignmentClassName =
     props.message.role === "user" ? "flex flex-row-reverse items-start" : "flex items-start";
   const styleClassName =
     props.message.role === "user" ? "flex rounded-b-xl rounded-tl-xl px-4  sm:min-h-0 sm:max-w-md md:max-w-2xl" : "flex rounded-b-xl rounded-tr-xl px-4 sm:max-w-md md:max-w-2xl";
+    const linkClassName =
+    props.message.role === "user" ? "text-white underline" : "underline text-blue-600 hover:text-blue-800";
+  
+    const options = { defaultProtocol: 'https',
+    className: linkClassName
+   };
+
   return (
 
     <div
@@ -15,12 +23,16 @@ export function ChatMessageBubble(props: { message: Message}) {
     >
       <div className={alignmentClassName}>
         {props.message.role === "user" ? null :
-            <CircularProfile active={true} type="chat"/>
+          <CircularProfile active={true} type="chat" />
         }
         <div
           className={`${styleClassName} ${colorClassName}`}
         >
-          <p className="chat-message-txt whitespace-pre-line">{props.message.content}</p>
+          <p className="chat-message-txt whitespace-pre-line">
+            <Linkify options={options}>
+              {props.message.content}
+            </Linkify>
+          </p>
         </div>
       </div>
     </div>
